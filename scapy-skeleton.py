24d974e44fe317920bbc81b84a6371918d9726c6
Tyler <tbbu225@g.uky.edu>
2019-04-09 12:26:59 -0400

    reworked callback for sniff()

--- scapy-skeleton.py
+++ scapy-skeleton.py
@@ -11,9 +11,12 @@ import json
 #Zhi Yuan
 """
 
+#arguments: label_number packet_count timout_val csv_to_write_to
 if len(sys.argv) < 5:
     raise Exception("not enough args")
 
+
+#create OrderedDict from list; elements are empty lists which will be pushed into by the callback function used for in sniff() 
 csv = OrderedDict()
 
 diction = ["Ethernet Src", "Ethernet Dst", "Ethernet Type", \
@@ -28,10 +31,12 @@ diction = ["Ethernet Src", "Ethernet Dst", "Ethernet Type", \
 for elem in diction:
     csv[elem] = []
 
+#define unknow value and get label number
 unknown = "???"
 
 label = int(sys.argv[1])
 
+#copy fields from packets into dictionary; use palceholder for NA values (ex: UDP fields when pakcets uses TCP) 
 def fields_extraction(x):
     if "Ethernet" in x: 
         csv["Ethernet Src"].append(x["Ethernet"].src)
@@ -42,7 +47,7 @@ def fields_extraction(x):
         csv["Ethernet Dst"].append(unknown)
         csv["Ethernet Type"].append(unknown)
     
-    if "IP" in x:
+    try:
         csv["IP Src"].append(x["IP"].src)
         csv["IP Dst"].append(x["IP"].dst)
         csv["IP Version"].append(x["IP"].version)
@@ -54,16 +59,8 @@ def fields_extraction(x):
         csv["IP frag"].append(x["IP"].frag)
         csv["IP ttl"].append(x["IP"].ttl)
         csv["IP proto"].append(x["IP"].proto)
-        csv["IP chksum"].append(x["IP"].chksum)        
-        csv["IPv6 Src"].append(unknown)
-        csv["IPv6 Dst"].append(unknown)
-        csv["IPv6 Version"].append(unknown)
-        csv["IPv6 tc"].append(unknown)
-        csv["IPv6 fl"].append(unknown)
-        csv["IPv6 plen"].append(unknown)
-        csv["IPv6 nh"].append(unknown)
-        csv["IPv6 hlim"].append(unknown)
-    elif "IPv6" in x:
+        csv["IP chksum"].append(x["IP"].chksum) 
+    except:
         csv["IP Src"].append(unknown)
         csv["IP Dst"].append(unknown)
         csv["IP Version"].append(unknown)
@@ -75,7 +72,9 @@ def fields_extraction(x):
         csv["IP frag"].append(unknown)
         csv["IP ttl"].append(unknown)
         csv["IP proto"].append(unknown)
-        csv["IP chksum"].append(unknown)        
+        csv["IP chksum"].append(unknown) 
+    
+    try:
         csv["IPv6 Src"].append(x["IPv6"].src)
         csv["IPv6 Dst"].append(x["IPv6"].dst)
         csv["IPv6 Version"].append(x["IPv6"].version)
@@ -84,19 +83,7 @@ def fields_extraction(x):
         csv["IPv6 plen"].append(x["IPv6"].plen)
         csv["IPv6 nh"].append(x["IPv6"].nh)
         csv["IPv6 hlim"].append(x["IPv6"].hlim)
-    else:
-        csv["IP Src"].append(unknown)
-        csv["IP Dst"].append(unknown)
-        csv["IP Version"].append(unknown)
-        csv["IP ihl"].append(unknown)
-        csv["IP tos"].append(unknown)
-        csv["IP len"].append(unknown)
-        csv["IP id"].append(unknown)
-        csv["IP flags"].append(unknown)
-        csv["IP frag"].append(unknown)
-        csv["IP ttl"].append(unknown)
-        csv["IP proto"].append(unknown)
-        csv["IP chksum"].append(unknown)        
+    except:
         csv["IPv6 Src"].append(unknown)
         csv["IPv6 Dst"].append(unknown)
         csv["IPv6 Version"].append(unknown)
@@ -105,8 +92,8 @@ def fields_extraction(x):
         csv["IPv6 plen"].append(unknown)
         csv["IPv6 nh"].append(unknown)
         csv["IPv6 hlim"].append(unknown)
-        
-    if "TCP" in x:
+    
+    try:
         csv["TCP sport"].append(x["TCP"].sport)
         csv["TCP dport"].append(x["TCP"].dport)
         csv["TCP seq"].append(x["TCP"].seq)
@@ -118,11 +105,7 @@ def fields_extraction(x):
         csv["TCP chksum"].append(x["TCP"].chksum)
         csv["TCP urgptr"].append(x["TCP"].urgptr)
         csv["TCP options"].append(x["TCP"].options)
-        csv["UDP sport"].append(unknown)
-        csv["UDP dport"].append(unknown)
-        csv["UDP len"].append(unknown)
-        csv["UDP chksum"].append(unknown)
-    elif "UDP" in x:
+    except:
         csv["TCP sport"].append(unknown)
         csv["TCP dport"].append(unknown)
         csv["TCP seq"].append(unknown)
@@ -134,39 +117,31 @@ def fields_extraction(x):
         csv["TCP chksum"].append(unknown)
         csv["TCP urgptr"].append(unknown)
         csv["TCP options"].append(unknown)
+    
+    try:
         csv["UDP sport"].append(x["UDP"].sport)
         csv["UDP dport"].append(x["UDP"].dport)
         csv["UDP len"].append(x["UDP"].len)
         csv["UDP chksum"].append(x["UDP"].chksum)
-    else:
-        csv["TCP sport"].append(unknown)
-        csv["TCP dport"].append(unknown)
-        csv["TCP seq"].append(unknown)
-        csv["TCP ack"].append(unknown)
-        csv["TCP dataofs"].append(unknown)
-        csv["TCP reserved"].append(unknown)
-        csv["TCP flags"].append(unknown)
-        csv["TCP window"].append(unknown)
-        csv["TCP chksum"].append(unknown)
-        csv["TCP urgptr"].append(unknown)
-        csv["TCP options"].append(unknown)
+    except:
         csv["UDP sport"].append(unknown)
         csv["UDP dport"].append(unknown)
         csv["UDP len"].append(unknown)
         csv["UDP chksum"].append(unknown)
-        
+    
     if "Raw" in x:
         csv["Raw"].append(x["Raw"].load)
     else:
         csv["Raw"].append(unknown)
-        
+    
     csv["time"].append(x.time)
     csv["Label"].append(label)
     
 
-
+#sniff packets
 pkts = sniff(prn = fields_extraction, filter = "(ip or ip6) && (tcp or udp)", count = int(sys.argv[2]), timeout = int(sys.argv[3]))
 
+#convert final dictionary to a dataframe and write it out to a csv
 pkt_frame = pandas.DataFrame().from_dict(csv)
 pkt_frame.to_csv("./" + sys.argv[4])
 
