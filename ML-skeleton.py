a5738845fee030f70bbc8cdd4a94be9c2db84636
awa294 <38330338+awa294@users.noreply.github.com>
2019-04-08 20:24:35 -0400

    using sample data

--- ML-skeleton.py
+++ ML-skeleton.py
@@ -2,19 +2,26 @@ import pandas as pd
 import numpy as np
 import csv 
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score 
+from sklearn.metrics import f1_score 
+from sklearn.metrics import precision_score 
+from sklearn.metrics import recall_score
 from sklearn.svm import SVC
 from sklearn.svm import LinearSVC
 from sklearn import tree
+from sklearn.neural_network import MLPClassifier
+import matplotlib.pyplot as plt
 
 
 
-df = pd.read_csv("data.csv", header=None)
+
+df = pd.read_csv("network_training.csv")
+#print(df[:10])
 # You might not need this next line if you do not care about losing information about flow_id etc. All you actually need to
 # feed your machine learning model are features and output label.
-columns_list = ['flow_id', 'IPsrc', 'IPdst', 'proto', 'feature_1', 'feature_2', 'feature_3', 'feature_4', 'feature_5', 'label']
+columns_list = ['tran_proto', 'avg_sent_len', 'avg_rec_len', 'avg_sent_ttl', 'average_rec_ttl', 'label']
 df.columns = columns_list
-features = ['proto', 'feature_1', 'feature_2', 'feature_3', 'feature_4', 'feature_5']
+features = ['tran_proto', 'avg_sent_len', 'avg_rec_len', 'avg_sent_ttl', 'average_rec_ttl']
 
 X = df[features]
 y = df['label']
@@ -22,20 +29,27 @@ y = df['label']
 acc_scores = 0
 for i in range(0, 10):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25)
+    results = []
 
     #Decision Trees
     clf = tree.DecisionTreeClassifier()
     clf.fit(X_train, y_train)
 
     # Neural network (MultiPerceptron Classifier)
-    # clf = MLPClassifier()
-    # clf.fit(X_train, y_train)
+    #clf = MLPClassifier()
+    #clf.fit(X_train, y_train)
 
     #SVM's
-    # clf = SVC(gamma='auto')     #SVC USE THIS
-    # clf = LinearSVC()  #Linear SVC
-    # clf.fit(X_train, y_train) 
+    #clf = SVC(gamma='auto')     #SVC USE THIS
+    #clf = LinearSVC()  #Linear SVC
+    #clf.fit(X_train, y_train) 
 
 
     #here you are supposed to calculate the evaluation measures indicated in the project proposal (accuracy, F-score etc)
-    result = clf.score(X_test, y_test)  #accuracy score
+    result = clf.score(X_test, y_test)  #accuracy score 
+    results.append(result)
+    print(result)
+    
+print("Average")
+avg= np.average(results)
+print(avg)
