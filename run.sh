0337c65bc2080d609025d70f5ade859703176d36
Tom Busby <tmbu225@uky.edu>
2019-04-13 11:28:21 -0400

    Fixed time. Added verbose messages.

--- run.sh
+++ run.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
-label=1
+if [ "$1" == "" ]; then
+	echo "No label provided. USAGE: ./run.sh [LABEL NUMBER]"
+	exit 1
+fi
+
+label=$1
+
+amount=1000
+ttl=128
+
+echo "Starting packet sniffing for label $label"
 
 for i in {1..10}
 do
-	sudo python scapy-skeleton.py $label 1000 128 ./tmp/sniff${i}.csv
+	echo "Sniff number: $i. $amount packets with $ttl ttl."
+	sudo python scapy-skeleton.py $label $amount $ttl ./tmp/sniff${i}.csv
+	echo "Written to ./tmp/sniff${i}.csv"
 done
 
-time=(date +"%H%M%S")
+time=$(date +"%H%M%S")
 
 touch ./result/result${time}.csv
 
@@ -15,5 +27,7 @@ echo "tran_proto, avg_sent_len, avg_rec_len, avg_sent_ttl, average_rec_ttl, labe
 
 for i in {1..10}
 do
+	echo "Processing ./tmp/sniff${i}.csv"
 	python processor.py ./tmp/sniff${i}.csv $label >> ./result/result${time}.csv
+	echo "Finished processing ./tmp/sniff${i}.csv. Data appended to ./result/result${time}.csv."
 done
