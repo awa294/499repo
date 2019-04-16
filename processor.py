ea549d1fbd3c927ff3170d1c14fa4c9ba14b745d
Tom Busby <tmbu225@uky.edu>
2019-04-15 20:15:29 -0400

    Quick fix for UDP ports.

--- processor.py
+++ processor.py
@@ -28,51 +28,57 @@ data = []
 
 # For every packet...
 for index, row in df.iterrows():
+
+	if (row['IP proto'] == 6):
+		cport = row['TCP sport']
+		dport = row['TCP dport']
+	elif (row['IP proto'] == 17):
+		cport = row['UDP sport']
+		dport = row['UDP dport']
+		
 	# Check to see if the ports and protocol are valid
-	if (row['TCP sport'] != "???") and (row['TCP dport'] != "???") and (row['IP proto'] != "???"):
-		if (not math.isnan(float(row['TCP sport']))) or (not math.isnan(float(row['TCP dport']))) or (not math.isnan(float(row['IP proto']))):
+	if (cport != "???") and (dport != "???") and (row['IP proto'] != "???"):
+		# Get the flow
+		flow = (row['IP Src'], row['IP Dst'], cport, dport, row['IP proto'])
 
-			# Get the flow
-			flow = (row['IP Src'], row['IP Dst'], row['TCP sport'], row['TCP dport'], row['IP proto'])
+		# Determine if the flow is going in or out
+		if(flow[0] == self_ip):
+			flow_ip = flow[1]
+			c_port = cport
+			s_port = dport
+			t_prot = row['IP proto']
+			sent = True
+		else:
+			flow_ip = flow[0]
+			c_port = dport
+			s_port = cport
+			t_prot = row['IP proto']
+			sent = False
 
-			# Determine if the flow is going in or out
-			if(flow[0] == self_ip):
-				flow_ip = flow[1]
-				c_port = row['TCP sport']
-				s_port = row['TCP dport']
-				t_prot = row['IP proto']
-				sent = True
-			else:
-				flow_ip = flow[0]
-				c_port = row['TCP dport']
-				s_port = row['TCP sport']
-				t_prot = row['IP proto']
-				sent = False
+		# If we haven't seen this flow from src to dest, add it to the list
+		if (flow_ip, c_port, s_port, t_prot) not in flows:
+			flows.append((flow_ip, c_port, s_port, t_prot))
+			data.append([t_prot, 0, 0, 0, 0, 0])
+		
+		# Get the index of the flow we're working with
+		index = flows.index((flow_ip, c_port, s_port, t_prot))
 
-			# If we haven't seen this flow from src to dest, add it to the list
-			if (flow_ip, c_port, s_port, t_prot) not in flows:
-				flows.append((flow_ip, c_port, s_port, t_prot))
-				data.append([t_prot, 0, 0, 0, 0, 0])
-			
-			# Get the index of the flow we're working with
-			index = flows.index((flow_ip, c_port, s_port, t_prot))
+		# Check validity if IP length and ttl
+		if not ((isinstance(row['IP len'], int) or (isinstance(row['IP len'], int)))):
+			row['IP len'] = '0'
+		if not ((isinstance(row['IP ttl'], int) or (isinstance(row['IP ttl'], int)))):
+			row['IP ttl'] = '0'
 
-			# Check validity if IP length and ttl
-			if not ((isinstance(row['IP len'], int) or (isinstance(row['IP len'], int)))):
-				row['IP len'] = '0'
-			if not ((isinstance(row['IP ttl'], int) or (isinstance(row['IP ttl'], int)))):
-				row['IP ttl'] = '0'
+		# Add the data to the relevent columns (Either in or out)
+		if sent:
+			data[index][1] += int(row['IP len'])
+			data[index][3] += int(row['IP ttl'])
+		else:
+			data[index][2] += int(row['IP len'])
+			data[index][4] += int(row['IP ttl'])
 
-			# Add the data to the relevent columns (Either in or out)
-			if sent:
-				data[index][1] += int(row['IP len'])
-				data[index][3] += int(row['IP ttl'])
-			else:
-				data[index][2] += int(row['IP len'])
-				data[index][4] += int(row['IP ttl'])
-
-			# Increment the the tracked amount of packets for a flow
-			data[index][5] += 1
+		# Increment the the tracked amount of packets for a flow
+		data[index][5] += 1
 
 
 for row in data:
